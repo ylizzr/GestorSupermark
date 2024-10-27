99518fba956c77a9b1939d64230ffc23f97a47d4
ylizzr <ylizarazor@ucentral.edu.co>
2024-10-26 20:41:43 -0500

    (nuevo:Estructura)

---
+++ src/main/java/co/ucentral/gestionSupermark/GestionSupermarkApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.gestionSupermark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionSupermarkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionSupermarkApplication.class, args);
+	}
+
+}
